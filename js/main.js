6c0a2e6dbe106282a2b03235f80f9e312e900cea
NandaGilang11 <nandagilang@students.amikom.ac.id>
2023-12-06 11:33:16 +0700

    Play-Movie

--- js/main.js
+++ js/main.js
@@ -37,3 +37,22 @@ var swiper = new Swiper(".popular-content", {
     },
   },
 });
+
+
+// Show Video
+let playButton = document.querySelector('.play-movie');
+let video = document.querySelector('.video-container');
+let myvideo = document.querySelector('#myvideo');
+let closebtn = document.querySelector('.close-video');
+
+playButton.onclick = () =>{
+  video.classList.add('show-video');
+  // Auto Play When Click on Buttton
+  myvideo.play();
+};
+
+closebtn.onclick = () => {
+  video.classList.remove("show-video");
+  // Pause on Close Video
+  myvideo.pause();
+};
